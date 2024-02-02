328c717cd1ad00b7f52c093e64ecde8916975b9f
camille <106037608+rirum@users.noreply.github.com>
2024-02-02 18:00:26 -0300

    fix: restarted project

---
+++ camille-m/src/app/global.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalCSS = createGlobalStyle`
+
+* {
+
+margin: 0;
+text-decoration: none;
+box-sizing: border-box;
+color:inherit;
+
+}
+
+`;
+
+export default GlobalCSS;
